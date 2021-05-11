379db4e525ec500905a61ded278ab64500dcaf0f
Kim HyunJi <khyunjiee@gmail.com>
2021-05-11 23:15:55 +0900

    update: 21/05/11 Count Primes

---
+++ src/may/CountPrimes.java
@@ -0,0 +1,35 @@
+package may;
+
+public class CountPrimes {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countPrimes(10));
+        System.out.println(solution.countPrimes(0));
+        System.out.println(solution.countPrimes(1));
+        System.out.println(solution.countPrimes(499979));
+    }
+}
+
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        int result = 0;
+
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+            if (!isPrime[i]) continue;
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) result++;
+        }
+
+        return result;
+    }
+}
