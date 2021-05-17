618043bc09998f772c1ec8de99e5e510bd269612
Kim HyunJi <khyunjiee@gmail.com>
2021-05-17 14:58:58 +0900

    update: 21/05/17 Binary Tree Cameras

---
+++ src/may/TreeNode.java
@@ -0,0 +1,39 @@
+package may;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class SolutionTreeNode {
+    int answer = 0;
+
+    public int minCameraCover(TreeNode root) {
+        return (dfs(root) == Monitoring.REQUIRED) ? answer + 1: answer;
+    }
+
+    private Monitoring dfs(TreeNode node) {
+        if (node == null) return Monitoring.NONE;
+        Monitoring leftType = dfs(node.left);
+        Monitoring rightType = dfs(node.right);
+
+        if (leftType == Monitoring.NONE && rightType == Monitoring.NONE) return Monitoring.REQUIRED;
+        else if ((leftType == Monitoring.MONITORING && rightType != Monitoring.REQUIRED) || (rightType == Monitoring.MONITORING && leftType != Monitoring.REQUIRED))
+            return Monitoring.NONE;
+        answer++;
+        return Monitoring.MONITORING;
+    }
+}
+
+enum Monitoring {
+    NONE, MONITORING, REQUIRED
+}
