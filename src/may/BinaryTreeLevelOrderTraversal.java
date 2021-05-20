04c35b09da06363a6ef1580a433bb224cda304b5
Kim HyunJi <khyunjiee@gmail.com>
2021-05-21 02:15:21 +0900

    update: 21/05/21 Binary Tree Level Order Traversal

---
+++ src/may/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,28 @@
+package may;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal {
+}
+
+class SolutionBinaryTreeLevelOrderTraversal {
+    List<List<Integer>> result;
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+         result = new ArrayList<>();
+         dfs(root, 0);
+
+         return result;
+    }
+
+    private void dfs(TreeNode node, int index) {
+        if (node == null) return;
+
+        if (result.size() <= index) result.add(new ArrayList<>());
+        result.get(index).add(node.val);
+
+        dfs(node.left, index + 1);
+        dfs(node.right, index + 1);
+    }
+}
