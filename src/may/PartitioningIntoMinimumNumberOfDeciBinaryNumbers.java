5523afcdd59fc74fa2a5e92376b9934eb1ea908f
Kim HyunJi <khyunjiee@gmail.com>
2021-05-26 16:43:52 +0900

    update: 21/05/26 Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ src/may/PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,18 @@
+package may;
+
+public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+}
+
+class SolutionPartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+    public int minPartitions(String n) {
+        String[] array = n.split("");
+        int max = 0;
+
+        for (String str: array) {
+            int num = Integer.parseInt(str);
+            if (num > max) max = num;
+        }
+
+        return max;
+    }
+}
