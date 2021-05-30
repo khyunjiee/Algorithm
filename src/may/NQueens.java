a118e61c90e613b1a901fb601fd6693ac167be2c
Kim HyunJi <khyunjiee@gmail.com>
2021-05-30 23:30:57 +0900

    update: 21/05/30 NQueens2

---
+++ src/may/NQueens.java
@@ -0,0 +1,49 @@
+package may;
+
+public class NQueens {
+    public static void main(String[] args) {
+        SolutionNQueens solution = new SolutionNQueens();
+//        System.out.println(solution.totalNQueens(4));
+        solution.totalNQueens(2);
+    }
+}
+
+class SolutionNQueens {
+    int result = 0;
+
+    // 0: 비어있음 1: 퀸 있음 -1: 퀸 못놓음
+    public int totalNQueens(int n) {
+        placeQueens(new int[n], 0, n);
+        return result;
+    }
+
+    private void placeQueens(int[] array, int row, int n) {
+        if (row == n) {
+            result++;
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            array[row] = i;
+            if (checkValidation(array, row)) {
+                pringArr(array);
+                placeQueens(array, row + 1, n);
+            }
+        }
+    }
+
+    private boolean checkValidation(int[] array, int row) {
+        for (int i = 0; i < row; i++) {
+            if (array[i] == array[row] || row - i == Math.abs(array[i] - array[row])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void pringArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
